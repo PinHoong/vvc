a6ac998e46121f25cf0b070a991cb9da7c48d982
PinHoong <e0725207@u.nus.edu>
2024-02-01 22:23:30 +0800

    Food choice button selection

--- src/components/VenueChoices.js
+++ src/components/VenueChoices.js
@@ -1,94 +1,112 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import ButtonBase from '@mui/material/ButtonBase';
-import Typography from '@mui/material/Typography';
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import ButtonBase from "@mui/material/ButtonBase";
+import Typography from "@mui/material/Typography";
 
 const images = [
   {
-    url: './randomtiger.jpeg',
-    title: 'Breakfast',
-    width: '40%',
+    url: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/00/a0000370/img/basic/a0000370_main.jpg?20201002142956&q=80",
+    title: "Japanese",
+    width: "33.3%",
   },
   {
-    url: '/static/images/buttons/burgers.jpg',
-    title: 'Burgers',
-    width: '30%',
+    url: "https://www.tastingtable.com/img/gallery/15-ingredients-that-will-seriously-elevate-your-steak/l-intro-1663169111.jpg",
+    title: "Western",
+    width: "33.3%",
   },
   {
-    url: '/static/images/buttons/camera.jpg',
-    title: 'Camera',
-    width: '30%',
+    url: "https://data.thefeedfeed.com/recommended/post_3122095.jpg",
+    title: "Korean",
+    width: "33.3%",
+  },
+  {
+    url: "https://www.dimsumcentral.com/wp-content/uploads/2018/06/what-is-dim-sum-header-new.jpg",
+    title: "Chinese",
+    width: "33.3%",
+  },
+  {
+    url: "https://www.tastingtable.com/img/gallery/20-italian-dishes-you-need-to-try-at-least-once/intro-1702481237.jpg",
+    title: "Italian",
+    width: "33.3%",
   },
 ];
 
 const ImageButton = styled(ButtonBase)(({ theme }) => ({
-  position: 'relative',
+  position: "relative",
   height: 200,
-  [theme.breakpoints.down('sm')]: {
-    width: '100% !important', // Overrides inline-style
+  [theme.breakpoints.down("sm")]: {
+    width: "100% !important", // Overrides inline-style
     height: 100,
   },
-  '&:hover, &.Mui-focusVisible': {
+  "&:hover, &.Mui-focusVisible": {
     zIndex: 1,
-    '& .MuiImageBackdrop-root': {
+    "& .MuiImageBackdrop-root": {
       opacity: 0.15,
     },
-    '& .MuiImageMarked-root': {
+    "& .MuiImageMarked-root": {
       opacity: 0,
     },
-    '& .MuiTypography-root': {
-      border: '4px solid currentColor',
+    "& .MuiTypography-root": {
+      border: "4px solid currentColor",
     },
   },
 }));
 
-const ImageSrc = styled('span')({
-  position: 'absolute',
+const ImageSrc = styled("span")({
+  position: "absolute",
   left: 0,
   right: 0,
   top: 0,
   bottom: 0,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center 40%',
+  backgroundSize: "cover",
+  backgroundPosition: "center 40%",
 });
 
-const Image = styled('span')(({ theme }) => ({
-  position: 'absolute',
+const Image = styled("span")(({ theme }) => ({
+  position: "absolute",
   left: 0,
   right: 0,
   top: 0,
   bottom: 0,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
   color: theme.palette.common.white,
 }));
 
-const ImageBackdrop = styled('span')(({ theme }) => ({
-  position: 'absolute',
+const ImageBackdrop = styled("span")(({ theme }) => ({
+  position: "absolute",
   left: 0,
   right: 0,
   top: 0,
   bottom: 0,
   backgroundColor: theme.palette.common.black,
   opacity: 0.4,
-  transition: theme.transitions.create('opacity'),
+  transition: theme.transitions.create("opacity"),
 }));
 
-const ImageMarked = styled('span')(({ theme }) => ({
+const ImageMarked = styled("span")(({ theme }) => ({
   height: 3,
   width: 18,
   backgroundColor: theme.palette.common.white,
-  position: 'absolute',
+  position: "absolute",
   bottom: -2,
-  left: 'calc(50% - 9px)',
-  transition: theme.transitions.create('opacity'),
+  left: "calc(50% - 9px)",
+  transition: theme.transitions.create("opacity"),
 }));
 
 export default function ButtonBaseDemo() {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const handleClick = (title) => {
+    setSelectedButton(title);
+  };
+
   return (
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
+    <Box
+      sx={{ display: "flex", flexWrap: "wrap", minWidth: 300, width: "100%" }}
+    >
       {images.map((image) => (
         <ImageButton
           focusRipple
@@ -96,8 +114,14 @@ export default function ButtonBaseDemo() {
           style={{
             width: image.width,
           }}
+          onClick={() => handleClick(image.title)}
         >
-          <ImageSrc style={{ backgroundImage: `url(${image.url})`, backgroundColor: 'white' }} />
+          <ImageSrc
+            style={{
+              backgroundImage: `url(${image.url})`,
+              backgroundColor: "white",
+            }}
+          />
           <ImageBackdrop className="MuiImageBackdrop-root" />
           <Image>
             <Typography
@@ -105,7 +129,7 @@ export default function ButtonBaseDemo() {
               variant="subtitle1"
               color="inherit"
               sx={{
-                position: 'relative',
+                position: "relative",
                 p: 4,
                 pt: 2,
                 pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
@@ -117,6 +141,7 @@ export default function ButtonBaseDemo() {
           </Image>
         </ImageButton>
       ))}
+      <Typography variant="body1">Selected button: {selectedButton}</Typography>
     </Box>
   );
 }
